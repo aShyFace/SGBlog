3ef69d57d57cc9626399ffb84b7fbf8cce670653
1146231094 <1146231094@qq.com>
2023-02-12 22:47:08 +0800

    创建项目

---
+++ code/SGBlog/sangeng-framework/src/main/java/com/example/mapper/ArticleMapper.java
@@ -0,0 +1,9 @@
+package com.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.domain.entity.Article;
+
+public interface ArticleMapper extends BaseMapper<Article> {
+
+
+}
