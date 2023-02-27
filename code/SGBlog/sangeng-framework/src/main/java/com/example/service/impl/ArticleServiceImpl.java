62f999de8dcaa32f0764a805755558abadf65a03
zhi <1146231094@qq.com>
2023-02-27 20:03:04 +0800

    成功编写第一个业务接口

--- code/SGBlog/sangeng-framework/src/main/java/com/example/service/impl/ArticleServiceImpl.java
+++ code/SGBlog/sangeng-framework/src/main/java/com/example/service/impl/ArticleServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.domain.ResponseResult;
 import com.example.mapper.ArticleMapper;
 import com.example.domain.entity.Article;
 import com.example.service.ArticleService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 文章表(Article)表服务实现类
@@ -15,4 +21,15 @@ import org.springframework.stereotype.Service;
 @Service("articleService")
 public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> implements ArticleService {
 
+    public ResponseResult<Article> hotArticalList() {
+        // 创建查询对象
+        LambdaQueryWrapper<Article> lqw = new LambdaQueryWrapper<Article>();
+        // 编写sql语句
+        lqw.eq(Article::getStatus, 0).orderByDesc(Article::getViewCount);
+        IPage page = new Page(1, 10);
+        // 执行sql语句
+        page(page, lqw);
+        List<Article> records = page.getRecords();
+        return ResponseResult.okResult(records);
+    }
 }
